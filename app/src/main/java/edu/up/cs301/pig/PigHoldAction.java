9e57f5627b893940b3d28b290bcb5408b17336e8
massad18 <massad18@up.edu>
2016-10-12 19:34:56 -0700

    CP2

---
+++ app/src/main/java/edu/up/cs301/pig/PigHoldAction.java
@@ -0,0 +1,14 @@
+package edu.up.cs301.pig;
+
+import edu.up.cs301.game.GamePlayer;
+import edu.up.cs301.game.actionMsg.GameAction;
+
+/**
+ * Created by massad18 on 10/12/2016.
+ */
+public class PigHoldAction extends GameAction {
+
+    public PigHoldAction(GamePlayer player) {
+        super(player);
+    }
+}
